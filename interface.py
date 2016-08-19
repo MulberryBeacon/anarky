683bd782e2643fcbf9cb220f6ca856b0b07c1d79
Eduardo Ferreira <mulberry.beacon@gmail.com>
2016-08-19 22:20:34 +0100

    Started updating the interface module.

--- interface.py
+++ interface.py
@@ -20,13 +20,13 @@ import sys
 
 # Constants
 # --------------------------------------------------------------------------------------------------
-ERROR = '{0} \'{1}\' is not available (doesn\'t exist or no privileges to access it)!'
+ERROR = '{} \'{}\' is not available (doesn\'t exist or no privileges to access it)!'
 
 
 # Project information
 # --------------------------------------------------------------------------------------------------
 __author__ = 'Eduardo Ferreira'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __license__ = 'MIT'
 
 
@@ -70,8 +70,6 @@ def get_input_files(entries):
     :param entries: The set of input entries (can be either files or directories)
     :return: A complete list of the input files
     """
-    #assert isinstance(entries, list), 'You need to provide a list of strings.'
-
     result = []
     for entry in entries:
         if isfile(entry):
@@ -82,7 +80,7 @@ def get_input_files(entries):
                     file_path = join(root, filename)
                     result.append(file_path)
         else:
-            _logger.error(ERROR.format('Filesystem entry', entry))
+            _logger.error(ERROR.format('File system entry', entry))
 
     return result
 
@@ -100,20 +98,26 @@ def parse_options(program, description, decode=False):
     # Defines the parent parser
     parser = argparse.ArgumentParser(prog=program, description=description)
     parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + __version__)
-    parser.add_argument('-p', '--playlist', action='store_true', default='False', help='create'\
-        'playlist file')
-    tags_help = '%s ID3 tags' % ('extract' if decode else 'add')
+    # TODO: hiding the playlist parameter because it'll only be tackled in a future version
+    #parser.add_argument('-p', '--playlist', action='store_true', default='False',
+    #    help='create playlist file')
+    tags_help = '{} ID3 tags'.format('extract' if decode else 'add')
     parser.add_argument('-t', '--tags', action='store_true', help=tags_help)
     cover_help = '{} album art'
     if decode:
-        parser.add_argument('-c', '--cover', action='store_true', help=cover_help.format('extract'))
+        parser.add_argument('-c', '--cover', action='store_true',
+            help=cover_help.format('extract'))
     else:
-        parser.add_argument('-c', '--cover', metavar='IMG', dest='cover', help=cover_help.format('add'))    
+        parser.add_argument('-c', '--cover', metavar='IMG', dest='cover',
+            help=cover_help.format('add'))    
 
     group = parser.add_argument_group('options')
-    group.add_argument('-f', '--files', nargs='+', metavar='FILES', dest='input_files', help='input files',
-                       required=True)
-    group.add_argument('-d', '--dest', metavar='DEST', dest='output_dir', help='output directory', required=True)
+    group.add_argument('-f', '--files', nargs='+', metavar='FILES', dest='input_files',
+        help='input files', required=True)
+    # TODO: the destination probably shouldn't be a required parameter. And the name could be
+    # changed to "output"...
+    group.add_argument('-d', '--dest', metavar='DEST', dest='output_dir', help='output directory',
+        required=True)
 
     return parser.parse_args()
 
@@ -132,11 +136,12 @@ def get_options(program, description, decode=False):
     files = get_input_files(args.input_files)
     if len(files) == 0:
         _logger.error('No files were provided!')
-        sys.exit()
+        sys.exit(1)
 
+    # TODO: this bit needs to be completely reviewed!
     # Checks the output directory, cover and tag parameters 
     if not (directory_exists(args.output_dir) and not (
             not decode and args.cover is not None and not file_exists(args.cover))):
-        sys.exit()
+        sys.exit(1)
 
     return files, args.output_dir, args.cover, args.tags, args.playlist
