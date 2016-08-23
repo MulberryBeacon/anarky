712664f74f53d25732fe8f10894d892b2c094b1c
Eduardo Ferreira <mulberry.beacon@gmail.com>
2016-08-23 23:32:11 +0100

    Fixed bugs in two methods of the interface library. Fixed the unit tests for the previous methods. Stored the library modules and unit test file in their new respective directories.

--- tests/interface_test.py
+++ tests/interface_test.py
@@ -10,20 +10,45 @@ License: MIT (see LICENSE for details)
 
 # Module import
 # --------------------------------------------------------------------------------------------------
-import interface
+import lib.interface as interface
+import tempfile
 import unittest
 
 
 # Test class
 # --------------------------------------------------------------------------------------------------
 class InterfaceTests(unittest.TestCase):
-    def test__file_exists(self):
-        with self.assertRaises(TypeError):
-            interface.file_exists(None)
+
+    # Tests for method "file_exists"
+    def test_file_exists_none(self):
+        result = interface.file_exists(None)
+        self.assertEqual(result, False)
+
+    def test_file_exists_empty(self):
+        result = interface.file_exists('')
+        self.assertEqual(result, True)
+
+    def test_file_exists(self):
+        temp_file = tempfile.mkstemp()[1]
+        result = interface.file_exists('')
+        self.assertEqual(result, True)
+
+    # Tests for method "directory_exists"
+    def test_directory_exists_none(self):
+        result = interface.directory_exists(None)
+        self.assertEqual(result, False)
+
+    def test_directory_exists_empty(self):
+        result = interface.directory_exists('')
+        self.assertEqual(result, True)
+
+    def test_directory_exists(self):
+        temp_directory = tempfile.mkdtemp()
+        result = interface.directory_exists(temp_directory)
+        self.assertEqual(result, True)
 
 
 # Methods :: Execution and boilerplate
-# -------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
     unittest.main()
-
