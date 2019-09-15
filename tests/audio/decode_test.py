54684e4422d36c6ea3c0bb3fab5af56002864690
Eduardo Ferreira <mulberry.beacon@gmail.com>
2019-09-15 20:37:36 +0100

    Added a dummy method for the encode and decode unit test files.

---
+++ tests/audio/decode_test.py
@@ -0,0 +1,22 @@
+# -*- coding: utf8 -*-
+
+"""
+Tests for the decoding library.
+
+Author: Eduardo Ferreira
+License: MIT (see LICENSE for details)
+"""
+
+import unittest
+
+
+class DecodeTests(unittest.TestCase):
+    """
+    Tests for the decoding library.
+    """
+
+    def test_dummy(self):
+        """
+        Dummy test method.
+        """
+        self.assertEqual(True, True)
---
+++ tests/audio/encode_test.py
@@ -0,0 +1,22 @@
+# -*- coding: utf8 -*-
+
+"""
+Tests for the encoding library.
+
+Author: Eduardo Ferreira
+License: MIT (see LICENSE for details)
+"""
+
+import unittest
+
+
+class EncodeTests(unittest.TestCase):
+    """
+    Tests for the encoding library.
+    """
+
+    def test_dummy(self):
+        """
+        Dummy test method.
+        """
+        self.assertEqual(True, True)
